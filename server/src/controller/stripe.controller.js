9286339f20e4f73875d18d8b415b4da3bd2d9aff
Claudia <claudx6@gmail.com>
2024-04-25 16:02:26 +0200

    stripe controller

--- server/src/controller/stripe.controller.js
+++ server/src/controller/stripe.controller.js
@@ -1,14 +1,13 @@
-// This is your test secret API key.
+// API key
 const stripe = require("stripe")('sk_test_51P8EEsCsMYx0cZJUCLRCVaeTwtJym6Gw1ir4rqIajrfimMlumuP36ThBQwqANRFAbCQ3e4HLknnNarcuzjnJy2N600VmsJs6iC');
 
 const createPaymentIntend = async (req,res) => {
     const { items } = req.body;
 
-    // Create a PaymentIntent with the order amount and currency
+    // Crear un PaymentIntent con el total del pedido y la moneda
     const paymentIntent = await stripe.paymentIntents.create({
       amount: 100,
       currency: "eur",
-      // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
       automatic_payment_methods: {
         enabled: true,
       },
